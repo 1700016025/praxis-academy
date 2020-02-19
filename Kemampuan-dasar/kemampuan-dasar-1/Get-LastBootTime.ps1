e344fca98d1ba9fb10aade235727bfdf48830c4f
1700016025 <dian1700016025@webmail.uad.ac.id>
2020-02-18 19:45:32 -1200

    dian

---
+++ Kemampuan-dasar/kemampuan-dasar-1/Get-LastBootTime.ps1
@@ -0,0 +1,8 @@
+﻿param(
+[string]$ComputerName
+)
+
+Get-WmiObject -Class Win32_OperatingSystem –ComputerName $ComputerName |
+Select-Object -Property CSName,@{n=”Last Booted”;
+
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
