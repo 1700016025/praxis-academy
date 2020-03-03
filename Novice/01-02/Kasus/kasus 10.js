077fd65cdf68d98d0841c07e3ac6e4190995c284
1700016025 <61131073+1700016025@users.noreply.github.com>
2020-03-03 14:59:55 +0700

    Add files via upload

---
+++ Novice/01-02/Kasus/kasus 10.js
@@ -0,0 +1,11 @@
+
+var x,y,chr;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
---
+++ Novice/01-02/Kasus/kasus 12.js
@@ -0,0 +1,9 @@
+var sum =0;
+for (var x  = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+        sum += x;
+    }
+}
+console.log(sum);
